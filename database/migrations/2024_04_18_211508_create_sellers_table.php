8c1217770a0a560a73040b6c272d7044f21a9a87
Fateme Atefrad <fa.atefrad@gmail.com>
2024-04-19 01:05:04 +0330

    [feat] seller and customer module migration and model prepared

--- database/migrations/2024_04_18_211508_create_sellers_table.php
+++ database/migrations/2024_04_18_211508_create_sellers_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('sellers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->boolean('is_active')->default(1);
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('password');
             $table->timestamps();
         });
     }
@@ -24,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('sellers');
     }
 };
