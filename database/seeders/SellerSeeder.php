a79f8fae4951a4eeeff237ac51c41cd961de1168
Fateme Atefrad <fa.atefrad@gmail.com>
2024-04-19 23:58:48 +0330

    [feat] seller seeder & factory added

---
+++ database/seeders/SellerSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Seller;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SellerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Seller::factory(10)->create();
+
+        Seller::factory()->create([
+            'name' => 'seller',
+            'email' => 'seller@example.com'
+        ]);
+    }
+}
