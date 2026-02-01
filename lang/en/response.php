bc71f3594403497934fa85f3b2f67471dca51c21
atefrad <f.atefrad@gmail.com>
2026-02-01 20:24:05 +0330

    Added English response translations for admin category

---
+++ lang/en/response.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'category_store_success' => 'Category created successfully.',
+    'category_update_success' => 'Category updated successfully.',
+    'category_delete_success' => 'Category deleted successfully.',
+];
